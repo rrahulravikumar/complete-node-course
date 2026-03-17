3fa31bc84aed5ec2677e441a6cf752eb782e7e8d
Rahul.Ravikumar <rahul.ravikumar@roxe.io>
2026-03-17 15:27:45 +0530

    second push

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 const textOut = `This is what we know about the avacado: b${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/input.txt", textOut);
 console.log("File written successfully");
+console.log("Working");
